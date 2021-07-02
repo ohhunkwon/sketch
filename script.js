dab01dd35aebb3b314678caeb41552319d029422
Ohhun Kwon <ohhunkwon@hotmail.com>
2021-07-02 12:04:20 +0100

    Added slider feature to implement adjustable sketchpad dimension

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const container = document.getElementById('container')
+const dimension = document.getElementById('dimension')
+
+window.addEventListener('load', createGrid(dimension.value))
+
+dimension.onchange = () => {
+    createGrid(dimension.value)
+}
 
 function createGrid(dimension) {
+    clearGrid()
+
     for (let i = 0; i < dimension ** 2; i++) {
         const cell = document.createElement('div')
         cell.classList.add('cell')
@@ -9,13 +18,17 @@ function createGrid(dimension) {
 
     container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`
     container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`
+
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => cell.addEventListener('mouseover', draw))
 }
 
 function draw() {
     this.classList.add('fill')
 }
 
-createGrid(30)
-
-const cells = document.querySelectorAll('.cell')
-cells.forEach(cell => cell.addEventListener('mouseover', draw))
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
+}
